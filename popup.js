5f3796e31b7347e048a417dfacc16e3ce39833db
Dion Almaer <dion@almaer.com>
2020-04-16 22:35:11 -0600

    #usetheplatform #useNewURL

--- popup.js
+++ popup.js
@@ -28,7 +28,7 @@ chrome.storage.local.get(null, function (items) {
     <div class="title"><a href="${item.url}" title="${item.url}">${
       item.title
     }</a></div>
-    <div class="domain">${extractDomain(item.url)}</div>
+    <div class="domain">${extractHostname(item.url)}</div>
   </div>
 `;
   }
@@ -42,22 +42,6 @@ clear.addEventListener("click", () => {
 });
 
 // A simple (read: won't work for everything) function to get a domain from a URL
-function extractDomain(url) {
-  let domain;
-  //find & remove protocol (http, ftp, etc.) and get domain
-  if (url.indexOf("://") > -1) {
-    domain = url.split("/")[2];
-  } else {
-    domain = url.split("/")[0];
-  }
-
-  //find & remove www
-  if (domain.indexOf("www.") > -1) {
-    domain = domain.split("www.")[1];
-  }
-
-  domain = domain.split(":")[0]; //find & remove port number
-  domain = domain.split("?")[0]; //find & remove url params
-
-  return domain;
+function extractHostname(url) {
+  return new URL(url).hostname;
 }
